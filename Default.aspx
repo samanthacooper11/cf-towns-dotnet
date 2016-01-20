2b57aa6ebcfd6f7dd2a072dfd282a0c91bba2943
samanthacooper11 <sacooper@pivotal.io>
2016-01-20 15:55:38 +0000

    first version

---
+++ Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            font-family: Arial;
+        }
+        .newStyle2 {
+            font-family: Arial;
+        }
+        .newStyle3 {
+            font-family: Arial;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1><span class="newStyle1">UK Cities</span></h1>
+        <br />
+    
+        <asp:Label ID="Label1" runat="server" Text="Label" CssClass="newStyle2"></asp:Label>
+    
+        <br />
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" CssClass="newStyle3" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private string envVariable;
+    private string connectionString;
+    private MySqlConnection mysql_connection;
+    private string mysqlHostname;
+    private string mysqlPort;
+    private string mysqlUsername;
+    private string mysqlPassword;
+    private string mysqldbname;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (getEnvVar())
+        {
+            initiateDB();
+            bindData();
+            
+        } else
+        {
+            Label1.Text = "Please bind to a MySql service";
+        } 
+    }
+
+    private bool getEnvVar()
+    {
+        //get environment variable
+        envVariable = Environment.GetEnvironmentVariable("VCAP_SERVICES");
+        dynamic jsonobj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(envVariable);
+
+        //check if MySql credentials are in the env var
+        try
+        {
+            var mysqlCredentails = jsonobj["p-mysql"][0];
+            mysqlHostname = mysqlCredentails.credentials.hostname.ToString();
+            mysqlPort = mysqlCredentails.credentials.port.ToString();
+            mysqlUsername = mysqlCredentails.credentials.username.ToString();
+            mysqlPassword = mysqlCredentails.credentials.password.ToString();
+            mysqldbname = mysqlCredentails.credentials.name.ToString();
+            //create connection string
+            connectionString = "server=" + mysqlHostname + ";uid=" + mysqlUsername + ";pwd=" + mysqlPassword + ";";
+            return true; 
+        }
+        catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException)
+        {
+            //no Mysql credentials within env var
+            return false;
+        }
+    }
+
+    private void initiateDB()
+    {
+        try
+        {
+            mysql_connection = new MySqlConnection(connectionString);
+            mysql_connection.Open();
+
+            string sqlStatement = "USE " + mysqldbname + ";";
+            MySqlCommand cmd = new MySqlCommand(sqlStatement, mysql_connection);
+            cmd.ExecuteNonQuery();
+            MySqlScript script1 = new MySqlScript(mysql_connection, File.ReadAllText("create-schema.sql"));
+            script1.Delimiter = "$$";
+            script1.Execute();
+            MySqlScript script2 = new MySqlScript(mysql_connection, File.ReadAllText("insert-towns.sql"));
+            script2.Delimiter = "$$";
+            script2.Execute();
+        }
+        catch (MySqlException ex)
+        {
+            Label1.Text = ex.Message.ToString();
+        }
+    
+    }
+
+    private void bindData()
+    {
+        using (mysql_connection)
+        {
+            MySqlDataAdapter adaptor = new MySqlDataAdapter("SELECT * from " + mysqldbname + ".uktowns", mysql_connection);
+            DataTable dt = new DataTable();
+            adaptor.Fill(dt);
+            if(dt.Rows.Count > 0)
+            {
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }     
+        }
+    }
+
+    private void closeConnection()
+    {
+        try
+        {
+            mysql_connection.Close();
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+}
